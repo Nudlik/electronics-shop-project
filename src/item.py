c6babfc8aa8b1e6df6f2ee9379cdc7faf2f02d25
Nudlik <gegorp@mail.com>
2023-08-23 18:49:42 +0700

    added methods in the class

--- src/item.py
+++ src/item.py
@@ -1,3 +1,9 @@
+import csv
+import os
+
+from settings import CSV_PATH
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,11 +19,73 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
-        self.price = price
-        self.quantity = quantity
+        self.__name = name
+        self.price = self.__validate_data(price)
+        self.quantity = self.__validate_data(quantity)
 
-        self.__class__.all.append(self)
+        self.all.append(self)
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        self.__name = self.__validate_name(name)
+
+    @classmethod
+    def __validate_name(cls, name: str) -> str:
+        """
+        Валидация имени товара не более 10 символов.
+
+        :param name: Наименование товара
+        :return: Обрезанное наименование товара
+        """
+        if len(name) > 10:
+            return name[:10]
+        return name
+
+    @classmethod
+    def __validate_data(cls, data: int | float | str) -> int | float:
+        """
+        Валидация данных для полей экземпляра класса.
+
+        :param data: Принимает данные типа int | float | str
+        :return: Преобразованные данные
+        """
+        if not (isinstance(data, int | float | str) or str(data).isdigit()):
+            raise ValueError('Неверный тип данных')
+        elif isinstance(data, str | float):
+            if data.count('.') == 1 or type(data) == float:
+                return float(data)
+            return int(data)
+
+    @classmethod
+    def instantiate_from_csv(cls) -> None:
+        """
+        Класс-метод, инициализирующий экземпляры класса Item данными из файла src/items.csv
+        и очищает текущие экземляры хранящиеся в all.
+
+        :return: None
+        """
+        if not os.path.exists(CSV_PATH):
+            raise FileNotFoundError(f'Отсутствует csv-файл по пути {CSV_PATH}')
+        with open(CSV_PATH) as file:
+            reader: csv.DictReader = csv.DictReader(file)
+            cls.all.clear()
+            [cls(**row) for row in reader]
+
+    @staticmethod
+    def string_to_number(string: str) -> int:
+        """
+        Статический метод, возвращающий число из числа-строки.
+
+        :param string: числовая строка
+        :return: целое число
+        """
+        if not (isinstance(string, int | float | str) or string.isdigit()):
+            raise ValueError('Неверный тип данных')
+        return int(float(string))
 
     def calculate_total_price(self) -> float:
         """
@@ -32,3 +100,6 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price = self.price * self.pay_rate
+
+    def __repr__(self):
+        return f'{__class__.__name__}(name="{self.__name}", price="{self.price}", quantity="{self.quantity}")'
